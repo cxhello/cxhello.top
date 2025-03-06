e7fdf01c4c9b49db131d60d9b4c90105e9f15377
cxhello <caixiaohuichn@gmail.com>
2025-03-06 20:19:00 +0800

    Update intro

--- components/home-page/intro.tsx
+++ components/home-page/intro.tsx
@@ -4,7 +4,7 @@ export function Intro() {
   return (
     <h1 className="text-neutral-900 dark:text-neutral-200">
       I'm <span className="font-medium">Cuthbert</span> - a passionate software engineer in
-      <span className="hidden font-medium">Viet Nam</span>
+      <span className="hidden font-medium">China</span>
       <span className="absolute ml-1.5 inline-flex pt-[3px]">
         <Twemoji emoji="flag-cn" />
       </span>
