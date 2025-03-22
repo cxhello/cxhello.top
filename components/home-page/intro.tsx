0e2fce1c31d1aad35f52b1807f2a1852c02d06ca
cxhello <caixiaohuichn@gmail.com>
2025-03-22 15:14:57 +0800

    feat: intro add space symbol

--- components/home-page/intro.tsx
+++ components/home-page/intro.tsx
@@ -3,7 +3,7 @@ import { Twemoji } from '~/components/ui/twemoji'
 export function Intro() {
   return (
     <h1 className="text-neutral-900 dark:text-neutral-200">
-      I'm <span className="font-medium">Cuthbert</span> - a passionate software engineer in
+      I'm <span className="font-medium">Cuthbert</span> - a passionate software engineer in 
       <span className="hidden font-medium">China</span>
       <span className="absolute ml-1.5 inline-flex pt-[3px]">
         <Twemoji emoji="flag-cn" />
